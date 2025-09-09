dc6f3903275acdea74fdbd5df6b6c2deb4789df8
unknown <zohaibjani019@gmail.com>
2025-09-09 10:45:59 -0700

    Fixed folder name case to models

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./Models/user.js");
+const User = require("./models/user.js");
 
 
 const listingRouter = require("./routes/listing.js");
